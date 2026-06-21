80d3731525e1359272279fef6638336d66ee891b
DHIWAHAR-K <adhithyak99@gmail.com>
2026-06-21 17:33:24 -0400

    Export task classes from tasks package init.

---
+++ src/tasks/__init__.py
@@ -0,0 +1,6 @@
+from src.tasks.base import BaseTask
+from src.tasks.parity import ParityTask
+from src.tasks.reasoning import ReasoningTask
+from src.tasks.translation import TranslationTask
+
+__all__ = ["BaseTask", "ParityTask", "ReasoningTask", "TranslationTask"]
